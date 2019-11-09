bd210b448a52270900a9e5d1ad0b9d71f43c4992
emmahoggett <emma.hoggett@epfl.ch>
2019-11-09 16:55:02 +0100

    work

---
+++ labs/ex08/work/helper.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""Some helper functions."""
+import os
+import shutil
+import numpy as np
+from scipy import misc
+
+
+def load_data():
+    """Load data and convert it to the metrics system."""
+    path_dataset = "faithful.csv"
+    data = np.loadtxt(path_dataset, delimiter=" ", skiprows=0)
+    return data
+
+
+def normalize_data(data):
+    """normalize the data by (x - mean(x)) / std(x)."""
+    mean_data = np.mean(data, axis=0)
+    data = data - mean_data
+    std_data = np.std(data)
+    data = data / std_data
+    return data
+
+
+def build_dir(dir):
+    """build a new dir. if it exists, remove it and build a new one."""
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+    os.makedirs(dir)
+
+
+def load_image(path):
+    """use the scipy.misc to load the image."""
+    return misc.imread(path)
+
+
+def build_distance_matrix(data, mu):
+    """build a distance matrix.
+    return
+        distance matrix:
+            row of the matrix represents the data point,
+            column of the matrix represents the k-th cluster.
+    """
+    
+    dist_list=[]
+    num_cluster, _ = mu.shape
+    
+    for k in range(num_cluster):
+        norm = np.sum(np.square(data - mu[k,:]), axis = 1)
+        dist_list.append(norm)
+        
+    return np.array(dist_list).T
